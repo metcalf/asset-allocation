425d0e7366db512ebee935f362e0ec35741d65c2
Andrew Metcalf <andrew@stripe.com>
2018-07-04 22:50:25 -0700

    cleaner optimization of asset location

--- analysis.py
+++ analysis.py
@@ -1,6 +1,8 @@
+from collections import defaultdict
+import copy
+
 import scipy.optimize
 import numpy as np
-from collections import defaultdict
 
 def accounts_to_allocations(accounts, asset_config, exclude_losses=False):
     allocations = defaultdict(float)
@@ -33,7 +35,7 @@ def sum_to_classes(allocations, num_classes):
         per_class[i % num_classes] += a
     return per_class
 
-def objective(allocations, targets, penalties):
+def objective(allocations, targets):
     if len(allocations) % len(targets) != 0:
         raise Exception(
             "Allocation variable vector (%d) is not a multiple of the length of targets (%d)" %(
@@ -43,12 +45,75 @@ def objective(allocations, targets, penalties):
 
     # Sum of the squares of the differences
     per_class = sum_to_classes(allocations, len(targets))    
-    diff = sum((t - a) ** 2 for t, a in zip(targets, per_class))
+    return sum((t - a) ** 2 for t, a in zip(targets, per_class))
 
-    for i, penalty in enumerate(penalties):
-        diff += allocations[i] * penalty
-    
-    return diff
+def location_objective(taxable_allocations, non_taxable_preference):
+    return sum(taxable_allocations[i] * pref for i, pref in enumerate(non_taxable_preference))
+
+def optimize_locations(allocations, classes, min_taxable_allocations, allowed_in_taxable):
+    constraints = []
+    num_classes = len(classes)
+
+    taxable_allocations = allocations[:num_classes]
+
+    # Account total doesn't change
+    taxable_total = sum(taxable_allocations)
+    constraints.append(scipy.optimize.LinearConstraint(
+        [1] * num_classes,
+        taxable_total, taxable_total
+    ))
+
+    class_totals = sum_to_classes(allocations, num_classes)
+
+    non_taxable_preference = [0] * num_classes
+    for i, c in enumerate(classes):
+        arr = [0] * num_classes
+        arr[i] = 1
+
+        total_for_class = class_totals[i]  
+
+        # TODO: dedupe
+        min_lim = min_taxable_allocations[i]      
+        if c in allowed_in_taxable:
+            max_lim = max(min_lim, total_for_class)
+        else:
+            max_lim = min_lim
+
+        constraints.append(scipy.optimize.LinearConstraint(
+            arr, min_lim, max_lim
+        ))    
+
+        if abs(total_for_class) < 1:
+            continue
+
+        if "bond" in c:
+            pref = 1
+        elif c == "Emerging international stock":
+            pref = 0.5
+        elif c == "Developed international stock":
+            pref = 0.25
+        else:
+            pref = 0
+        non_taxable_preference[i] = pref / total_for_class
+
+    soln = scipy.optimize.minimize(
+        location_objective,
+        taxable_allocations,
+        args=(non_taxable_preference,),
+        jac='2-point', # TODO: `gradient
+        hess=scipy.optimize.BFGS(), # TODO: Pretty sure I can provide this
+        method='trust-constr',
+        constraints=constraints,
+        options={"maxiter": 5000},
+    )
+
+    if abs(soln.optimality) > 1:
+        raise Exception("Expected optimality <<1, got %f" % soln.optimality)
+
+    new_non_taxable = [class_totals[i] - soln.x[i] for i in range(num_classes)]
+    new_allocations = np.concatenate((soln.x, new_non_taxable))
+
+    return new_allocations
 
 def optimize_allocations(taxable_accts, non_taxable_accts, classes, assets, targets):
     num_classes = len(classes)
@@ -112,43 +177,12 @@ def optimize_allocations(taxable_accts, non_taxable_accts, classes, assets, targ
             arr, min_lim, max_lim
         ))
 
-    # Constrain over-investing in an asset class.
-    # This can happen when we apply adjustments to the objective function to steer
-    # allocations between taxable/non-taxable
-    # TODO: It's not clear that these adjustments/penalties are a good ideas anyway
-    # versus some other way of later reshuffling between asset classes (e.g. like a second
-    # optimization pass)
-    # Anyway, this totally breaks things
-    # for i in range(num_classes):
-    #     arr = [0] * (2 * num_classes)
-    #     arr[i] = 1
-    #     arr[num_classes + i] = 1
-
-    #     max_lim = max(min_taxable_allocations[i], target_allocations_vector[i])
-   
-    #     constraints.append(scipy.optimize.LinearConstraint(
-    #         arr, 0, max_lim
-    #     ))
-
-    penalties = [0] * (num_classes * 2)
-    if non_taxable_total > 0:
-        # Apply a small penalty to holding bonds in taxable accounts
-        for i, c in enumerate(classes):
-            if "bond" in c and target_allocations_vector[i] > 0:
-                penalties[i] = 1.0 / target_allocations_vector[i]
-        # Apply a small preference for emerging stock in non-taxable accounts to hold in Roth
-        # and a smaller preference for developed international. This also helps sequence what
-        # goes into which account to reduce churn
-    
-        penalties[num_classes + classes.index("Emerging international stock")] = - 2.0 / non_taxable_total
-        penalties[num_classes + classes.index("Developed international stock")] = -1.0 / non_taxable_total
-
     current_allocations = current_taxable_allocations + current_non_taxable_allocations
 
     soln = scipy.optimize.minimize(
         objective,
         current_allocations,
-        args=(target_allocations_vector, penalties),
+        args=(target_allocations_vector,),
         jac='3-point', # TODO: `gradient
         hess=scipy.optimize.BFGS(), # TODO: Pretty sure I can provide this
         method='trust-constr',
@@ -159,7 +193,14 @@ def optimize_allocations(taxable_accts, non_taxable_accts, classes, assets, targ
     if abs(soln.optimality) > 1:
         raise Exception("Expected optimality <<1, got %f" % soln.optimality)
 
-    if abs(sum(soln.x) - total) > 1:
+    located_allocations = optimize_locations(
+        allocations=soln.x, 
+        classes=classes,
+        min_taxable_allocations=min_taxable_allocations,
+        allowed_in_taxable=allowed_in_taxable
+    )
+
+    if abs(sum(located_allocations) - total) > 1:
         raise Exception("Solution didn't spend all the money!")
 
-    return (current_allocations, soln.x)
+    return (current_allocations, located_allocations)
