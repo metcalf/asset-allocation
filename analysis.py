be546477da5cfa198021b37ec7d5fa9771e6d66f
Andrew Metcalf <andrew@stripe.com>
2019-04-05 11:25:49 -0700

    avoid unnecessary selling

--- analysis.py
+++ analysis.py
@@ -26,7 +26,7 @@ def find_allowed_classes(assets, location):
     for asset in assets.values():
         if asset.get("location", location) == location:
             allowed.add(asset["class"])
-    
+
     return allowed
 
 def sum_to_classes(allocations, num_classes):
@@ -44,7 +44,7 @@ def objective(allocations, targets):
         )
 
     # Sum of the squares of the differences
-    per_class = sum_to_classes(allocations, len(targets))    
+    per_class = sum_to_classes(allocations, len(targets))
     return sum((t - a) ** 2 for t, a in zip(targets, per_class))
 
 def location_objective(taxable_allocations, non_taxable_preference):
@@ -70,10 +70,10 @@ def optimize_locations(allocations, classes, min_taxable_allocations, allowed_in
         arr = [0] * num_classes
         arr[i] = 1
 
-        total_for_class = class_totals[i]  
+        total_for_class = class_totals[i]
 
         # TODO: dedupe
-        min_lim = min_taxable_allocations[i]      
+        min_lim = min_taxable_allocations[i]
         if c in allowed_in_taxable:
             max_lim = max(min_lim, total_for_class)
         else:
@@ -81,7 +81,7 @@ def optimize_locations(allocations, classes, min_taxable_allocations, allowed_in
 
         constraints.append(scipy.optimize.LinearConstraint(
             arr, min_lim, max_lim
-        ))    
+        ))
 
         if abs(total_for_class) < 1:
             continue
@@ -113,9 +113,23 @@ def optimize_locations(allocations, classes, min_taxable_allocations, allowed_in
 
     return new_allocations
 
+
+def run_optimization(current_allocations, target_allocations_vector, constraints):
+    return scipy.optimize.minimize(
+        objective,
+        current_allocations,
+        args=(target_allocations_vector,),
+        jac='3-point', # TODO: `gradient
+        hess=scipy.optimize.BFGS(), # TODO: Pretty sure I can provide this
+        method='trust-constr',
+        constraints=constraints,
+        options={"maxiter": 5000},
+    )
+
+
 def optimize_allocations(taxable_accts, non_taxable_accts, classes, assets, targets, no_sell_holdings, allow_gains):
     num_classes = len(classes)
-    
+
     current_taxable_allocations = class_vector(classes, assets, taxable_accts)
     current_non_taxable_allocations = class_vector(classes, assets, non_taxable_accts)
 
@@ -123,11 +137,11 @@ def optimize_allocations(taxable_accts, non_taxable_accts, classes, assets, targ
     # TODO: What we really want is to net the negative amounts of a given holding out against the positives
     # and then filter on what remains
     min_taxable_allocations = class_vector(
-        classes, assets, taxable_accts, 
+        classes, assets, taxable_accts,
         filter=lambda acct, hldg: hldg.value < 0 or hldg.symbol in no_sell_holdings[acct.name] or (hldg.value > hldg.basis and not allow_gains)
     )
     min_non_taxable_allocations = class_vector(
-        classes, assets, non_taxable_accts, 
+        classes, assets, non_taxable_accts,
         filter=lambda acct, hldg: hldg.value < 0 or hldg.symbol in no_sell_holdings[acct.name]
     )
 
@@ -150,7 +164,7 @@ def optimize_allocations(taxable_accts, non_taxable_accts, classes, assets, targ
         if c in allowed_in_non_taxable and c not in allowed_in_taxable:
             total_only_in_taxable += target_allocations_vector[i]
     if total_only_in_taxable + target_allocations_vector[classes.index("US bond")] <= non_taxable_total:
-        allowed_in_taxable.remove("US bond")        
+        allowed_in_taxable.remove("US bond")
 
     constraints = []
 
@@ -197,20 +211,36 @@ def optimize_allocations(taxable_accts, non_taxable_accts, classes, assets, targ
 
     current_allocations = current_taxable_allocations + current_non_taxable_allocations
 
-    soln = scipy.optimize.minimize(
-        objective,
-        current_allocations,
-        args=(target_allocations_vector,),
-        jac='3-point', # TODO: `gradient
-        hess=scipy.optimize.BFGS(), # TODO: Pretty sure I can provide this
-        method='trust-constr',
-        constraints=constraints,
-        options={"maxiter": 5000},
-    )
+    best_soln = run_optimization(current_allocations, target_allocations_vector, constraints)
 
-    if abs(soln.optimality) > 1:
-        raise Exception("Expected optimality <<1, got %f" % soln.optimality)
-    allocations = soln.x
+    # Attempt to avoid cases where we sell an asset only to buy in another account
+    for i, v in enumerate(best_soln.x):
+        if (v - current_allocations[i]) >= 0:
+            continue
+        if current_allocations[i] == 0 and (target_allocations_vector[i % num_classes]) == 0:
+            continue
+
+        arr = [0] * (2 * num_classes)
+        arr[i] = 1
+        new_constraints = constraints + [
+            scipy.optimize.LinearConstraint(
+                arr, current_allocations[i], current_allocations[i],
+            ),
+
+        ]
+        curr_soln = run_optimization(current_allocations, target_allocations_vector, new_constraints)
+        if (curr_soln.fun - best_soln.fun) < 0.1:
+            best_soln = curr_soln
+            constraints = new_constraints
+            print("+", end="", flush=True)
+        else:
+            print(".", end="", flush=True)
+
+    print("")
+
+    if abs(best_soln.optimality) > 1:
+        raise Exception("Expected optimality <<1, got %f" % best_soln.optimality)
+    allocations = best_soln.x
 
     if abs(sum(allocations) - total) > 1:
         raise Exception("Solution didn't spend all the money!")
